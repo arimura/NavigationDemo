51cd6b08ff1323211960dca84eea14a56a9bb54f
arimura <kotaro_arimura@voyagegroup.com>
2023-01-18 11:08:02 +0900

    Make navbar opaque

--- NavigationDemo/NavigationDemo/ViewController.swift
+++ NavigationDemo/NavigationDemo/ViewController.swift
@@ -12,8 +12,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.configureWithOpaqueBackground()
+        
+        let navBar = self.navigationController?.navigationBar
+        navBar?.compactAppearance = navigationBarAppearance
+        navBar?.standardAppearance = navigationBarAppearance
+        navBar?.scrollEdgeAppearance = navigationBarAppearance
+        navBar?.compactScrollEdgeAppearance = navigationBarAppearance
     }
-
-
 }
 
